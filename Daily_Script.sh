3c4f26b8f11131a0d3ef95d1f1b507e2331682ab
Matt Packwood <mattpackwood@gmail.com>
2020-05-19 16:20:46 -0400

    File path
    Moved to Documents from Desktop

--- Daily_Script.sh
+++ Daily_Script.sh
@@ -2,5 +2,5 @@
 date_stamp=$(date "+BackUp-%Y-%m-%d")
 echo "Bookmarks.plist-"$date_stamp
 
-cp /Users/mattpackwood/Library/Safari/Bookmarks.plist /Users/mattpackwood/Desktop/Safari\ Bookmarks/"Bookmarks.plist-"$date_stamp
+cp /Users/mattpackwood/Library/Safari/Bookmarks.plist /Users/mattpackwood/Documents/Safari\ Bookmarks/"Bookmarks.plist-"$date_stamp
 
