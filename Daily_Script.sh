e31216397f78faada8b1ff7f5e9833d32926c807
Matt Packwood <mattpackwood@gmail.com>
2020-07-22 14:27:24 -0400

    Fixing paths
    Moved GitHub files out of iCloud

--- Daily_Script.sh
+++ Daily_Script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 date_stamp=$(date "+BackUp-%Y-%m-%d")
 echo "Bookmarks.plist-"$date_stamp
-osascript /Users/mattpackwood/Documents/GitHub/Bookmark_BackUp/Back_Up_Safari.scpt
+osascript /Users/mattpackwood/GitHub/Bookmark_BackUp/Back_Up_Safari.scpt
 cp /Users/mattpackwood/Library/Safari/Bookmarks.plist /Users/mattpackwood/Documents/Safari\ Bookmarks/"Bookmarks.plist-"$date_stamp
 
